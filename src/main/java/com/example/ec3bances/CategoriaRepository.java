eef7565a745191a784267d8c6f278a82a67c7a55
AlexBAlcantara14 <135645103+AlexBAlcantara14@users.noreply.github.com>
2023-07-24 19:12:41 +0000

    EC3 BANCES

--- src/main/java/com/example/ec3bances/CategoriaRepository.java
+++ src/main/java/com/example/ec3bances/CategoriaRepository.java
@@ -1,9 +1,13 @@
 package com.example.ec3bances;
-
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import java.util.List;
 
-// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
-// CRUD refers Create, Read, Update, Delete
+@RepositoryRestResource(collectionResourceRel = "categorias", path = "categorias")
+public interface CategoriaRepository extends PagingAndSortingRepository<Categoria, Integer>,CrudRepository<Categoria, Integer> {
 
-public interface CategoriaRepository extends CrudRepository<Categoria, Integer> {
+    List<Categoria> findByNombre(@Param("nombre") String nombre);
+
 }
--- src/main/java/com/example/ec3bances/InstrumentoRepository.java
+++ src/main/java/com/example/ec3bances/InstrumentoRepository.java
@@ -1,9 +1,14 @@
 package com.example.ec3bances;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import java.util.List;
 
-// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
-// CRUD refers Create, Read, Update, Delete
+@RepositoryRestResource(collectionResourceRel = "instrumentos", path = "instrumentos")
+public interface InstrumentoRepository extends PagingAndSortingRepository<Instrumento, Integer>,CrudRepository<Instrumento, Integer> {
 
-public interface InstrumentoRepository extends CrudRepository<Instrumento, Integer> {
+    List<Instrumento> findByNombre(@Param("nombre") String nombre);
+
 }
